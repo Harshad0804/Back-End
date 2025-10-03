7b055eea7c9b7ec2ac1892790217bee19aafe617
Harshad0804 <harshadkarguwal@gmail.com>
2025-10-04 00:07:24 +0530

    Python

---
+++ ASSIGNMENT/Python - Fundamentals Of Python Language/LAB-Exercise/Code-Structure.py
@@ -0,0 +1,12 @@
+def greet_user(name):
+    """Function to greet the user."""
+    print(f"Hello, {name}! Welcome to Python.")
+
+def main():
+    """Main function of the program."""
+    user_name = "Harshad"   # variable inside main
+    greet_user(user_name)   # calling function
+
+# Program execution starts here
+if __name__ == "__main__":
+    main()
