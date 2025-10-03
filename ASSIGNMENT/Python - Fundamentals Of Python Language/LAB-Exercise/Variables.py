f2c79b2cf0160bcf03e5ea01b4076e874501b09c
Harshad0804 <harshadkarguwal@gmail.com>
2025-10-04 00:08:57 +0530

    Python

---
+++ ASSIGNMENT/Python - Fundamentals Of Python Language/LAB-Exercise/Variables.py
@@ -0,0 +1,17 @@
+# Integer variable
+age = 21
+
+# Float variable
+height = 5.9
+
+# String variable
+name = "Harshad"
+
+# Boolean variable
+is_student = True
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student?", is_student)
