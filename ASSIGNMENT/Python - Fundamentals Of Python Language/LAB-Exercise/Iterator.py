1ac0dda76028bd8eb4abace589e1fab497458442
Harshad0804 <harshadkarguwal@gmail.com>
2025-10-06 13:41:06 +0530

    Python

---
+++ ASSIGNMENT/Python - Fundamentals Of Python Language/LAB-Exercise/Iterator.py
@@ -0,0 +1,22 @@
+class MyIterator:
+    def __init__(self, numbers):
+        self.numbers = numbers
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.numbers):
+            num = self.numbers[self.index]
+            self.index += 1
+            return num
+        else:
+            raise StopIteration
+
+nums = [1, 2, 3, 4, 5]
+
+it = MyIterator(nums)
+
+for n in it:
+    print(n)
