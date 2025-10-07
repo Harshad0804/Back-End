cc8b4eac0a9121dad749b13b7eb7700ba4375944
Harshad0804 <harshadkarguwal@gmail.com>
2025-10-07 18:06:47 +0530

    Python

---
+++ ASSIGNMENT/Python - Fundamentals Of Python Language/LAB-Exercise/Lab-9/String-Slicing.py
@@ -0,0 +1,13 @@
+text = "Python Programming"
+
+print(text[:2:5])
+print(text[15::5])
+print(text[1:3:3])
+print(text[::16])
+print(text[10:16])
+
+print(text[:-17:17])
+print(text[-15::10])
+print(text[-16:-14:5])
+print(text[::-9])
+print(text[-13:-7])
